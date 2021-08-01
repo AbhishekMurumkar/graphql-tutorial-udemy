4afd8c1a875df1fe23955199f0f517fa3bb17644
abhishekm <abhishekm@decisionengines.ai>
2021-08-01 12:43:11 +0530

    Completed building relationship among comments and posts

--- graphql-course/graphql-basics/src/index.js
+++ graphql-course/graphql-basics/src/index.js
@@ -2,12 +2,12 @@ import { GraphQLServer } from "graphql-yoga";
 
 /**
  * --------------------------- Setting Comments Data---------------------------------
- * Part 1: 
+ * Part 1: Build comments
  *  1. setup comments type definition where it must have an id(doesnt match any other ids for now) and text as string, both as non-nullable
  *  2. create new sample data for comments according to type definition with 5 elements
  *  3. setup a query handler that returns all comments upon query
  * 
- * Part 2:
+ * Part 2: Build relation ship among Comments and User
  *  1. Setup author field id in comment
  *  2. update all comments array to have an author field (use on the user ids as value)
  *  3. create a resolver for comments fields that return user who wrote the comment
@@ -15,6 +15,15 @@ import { GraphQLServer } from "graphql-yoga";
  *  5. set up comments field on user
  *  6. set up resolver that gets all user comments 
  *  7. run a sample query that gets all users and their comments
+ * 
+ * Part 3: build relationship among Posts and Comments
+ *  1. setup a post field in comments
+ *  2. update all comments in the array to have a new post field(use one of the post ids as value)
+ *  3. create a resolver for comments that returns post that comment belongs to
+ *  4. run a sample query that gets all comments and post names
+ *  5. set up a comment field on post
+ *  6. set up resolver for post fields that returns all comments belonging to the post
+ *  7. run a sample query that gets all posts and all their comments
  */
 
 // Demo comments data
@@ -22,22 +31,26 @@ const comments=[
   {
     id:'comments_1',
     text:'nice..!!',
-    author:'3'
+    author:'3',
+    post:'1',
   },
   {
     id:'comments_2',
     text:"shit..!!",
-    author:'2'
+    author:'2',
+    post:'4'
   },
   {
     id:'comments_3',
     text:'not so great, not so terrible',
-    author:'4'
+    author:'4',
+    post:'2'
   },
   {
     id:'comments_4',
     text:'always a loser',
-    author:'1'
+    author:'1',
+    post:'1'
   }
 ];
 // Demo Posts data
@@ -47,21 +60,24 @@ const posts=[
     title: 'First Post',
     body: 'Content of the post',
     published: false,
-    author:'1' 
+    author:'1',
+    comments:['1','4']
   },
   {
-    id: '1',
+    id: '2',
     title: 'Some Post',
     body: 'Content...!!!',
     published: true,
-    author:'2' 
+    author:'2',
+    comments:['3']
   },
   {
     id: '4',
     title: 'Demo Post',
     body: 'Content of the post',
     published: false,
-    author:'4' 
+    author:'4',
+    comments:['2']
   }
 ]
 //Demo Users data
@@ -103,7 +119,7 @@ type User{
  name: String!
  email:String!
  age: Int,
- posts:[Post!]!
+ posts:[Post!]
  comments:[Comments]
 } 
 type Post{
@@ -112,11 +128,13 @@ type Post{
   body:String!
   published:Boolean!
   author:User! #relational data
+  comments:[Comments]
 }
 type Comments{
   id:ID!,
   text:String!,
   author:User!
+  post:Post!
 }
 `;
 
@@ -130,6 +148,9 @@ const resolvers = {
     author(parent,args,ctx,info){
       console.log("parent",parent);
       return users.find(e=>e.id==parent.author);
+    },
+    post(parent,args,ctx,info){
+      return posts.find(e=>e.id==parent.post);
     }
   },
   // Users and posts are having 1-to-M bi-directional-relationship
@@ -141,6 +162,9 @@ const resolvers = {
       // we need to access field 'author' parent present in post
       // thus parent denotes 'post' object 
       return users.find(e=>e.id==parent.author);
+    },
+    comments(parent,args,ctx,info){
+      return comments.filter(e=>e.post==parent.id);
     }
   },
   User:{
@@ -190,6 +214,121 @@ server.start(() => {
   console.log("Server is started at port 4000");
 });
 // for clear understading , you can study below inputs and outputs from bottom to top order
+//----------------------------Fetching all posts and comments present in it.
+//QueryInput
+/**
+ * query{
+  post{
+    title
+    body
+    comments{
+      text
+    }
+  }
+}
+ */
+//Query Output
+/**
+ * {
+  "data": {
+    "post": [
+      {
+        "title": "First Post",
+        "body": "Content of the post",
+        "comments": [
+          {
+            "text": "nice..!!"
+          },
+          {
+            "text": "always a loser"
+          }
+        ]
+      },
+      {
+        "title": "Some Post",
+        "body": "Content...!!!",
+        "comments": [
+          {
+            "text": "not so great, not so terrible"
+          }
+        ]
+      },
+      {
+        "title": "Demo Post",
+        "body": "Content of the post",
+        "comments": [
+          {
+            "text": "shit..!!"
+          }
+        ]
+      }
+    ]
+  }
+}
+ */
+//----------------------------Fetching all comments and posts via comments
+//Query Input
+/**
+ * query{
+  comments{
+    text
+    author{
+      name
+    }
+    post{
+      title
+      body
+    }
+  }
+}
+ */
+//Query Output
+/**{
+  "data": {
+    "comments": [
+      {
+        "text": "nice..!!",
+        "author": {
+          "name": "Jeevana"
+        },
+        "post": {
+          "title": "First Post",
+          "body": "Content of the post"
+        }
+      },
+      {
+        "text": "shit..!!",
+        "author": {
+          "name": "Nagarjuna"
+        },
+        "post": {
+          "title": "Demo Post",
+          "body": "Content of the post"
+        }
+      },
+      {
+        "text": "not so great, not so terrible",
+        "author": {
+          "name": "Divya"
+        },
+        "post": {
+          "title": "Some Post",
+          "body": "Content...!!!"
+        }
+      },
+      {
+        "text": "always a loser",
+        "author": {
+          "name": "Abhishek"
+        },
+        "post": {
+          "title": "First Post",
+          "body": "Content of the post"
+        }
+      }
+    ]
+  }
+} */
 //----------------------------Fetching all users and their comments
 //Query Input
 /**
